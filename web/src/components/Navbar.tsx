9473b547b4a8aacd2d1fe2ecc8392d046cb050f1
Alex <alex.gillespie@live.com>
2022-05-17 16:21:25 -0400

    cache handling for login & register

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,8 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
-	return <Box bg='tomato'>pretty good</Box>;
+	const [{ data, fetching }] = useMeQuery();
+	let body = null;
+
+	if (fetching) {
+	} else if (!data?.me) {
+		<>
+			<NextLink href='/login'>
+				<Link mr={2}>Login</Link>
+			</NextLink>
+			<NextLink href='/register'>
+				<Link>Register</Link>
+			</NextLink>
+		</>;
+	} else {
+		body = (
+			<Flex>
+				<Box mr={2}>{data.me.username}</Box>
+				<Button variant='link'>logout</Button>
+			</Flex>
+		);
+	}
+	return (
+		<Flex bg='tomato' p={4}>
+			<Box ml={"auto"}>{body}</Box>
+		</Flex>
+	);
 };
