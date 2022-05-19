50a7895ae4abc149c1a12bdec995233dae807711
Alex <alex.gillespie@live.com>
2022-05-19 11:18:27 -0400

    password recovery setup

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,21 @@
 import { Navbar } from "../components/Navbar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-	<>
-		<Navbar />
-		<div>Hello World</div>
-	</>
-);
+const Index = () => {
+	const [{ data }] = usePostsQuery();
+	return (
+		<>
+			<Navbar />
+			<div>Hello World</div>
+			{!data ? (
+				<div>loading...</div>
+			) : (
+				data.posts.map(p => <div key={p.id}>{p.title}</div>)
+			)}
+		</>
+	);
+};
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
